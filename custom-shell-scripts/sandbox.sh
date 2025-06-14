973e55b4435d1c4afbe9459ce5cc9e62932f17cc
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2025-06-14 19:39:47 +0530

    [Abhishek] Refactoring

--- custom-shell-scripts/sandbox.sh
+++ custom-shell-scripts/sandbox.sh
@@ -7,22 +7,42 @@ sandbox() {
     docker run -it --rm -v "$PWD/$rand_dir":/workspace/sandbox:rw -w /workspace/sandbox abhishek1009/sandbox zsh
 }
 
-# Bind Ctrl+b to run sandbox() in supported shells (bash/zsh)
-if [[ -n $ZSH_VERSION ]]; then
-  bindkey -s '^B' 'sandbox\n'
-elif [[ -n $BASH_VERSION ]]; then
-  bind -x '"\C-b":sandbox'
-fi
-
 sandbox_curr() {
     curr_dir_name="$(basename "$PWD")"
     docker pull abhishek1009/sandbox:latest
     docker run -it --rm -v "$PWD":/workspace/"$curr_dir_name":rw -w /workspace/"$curr_dir_name" abhishek1009/sandbox zsh
 }
 
-# Bind Ctrl+h to run sandbox_curr() in supported shells (bash/zsh)
+sandbox_dev() {
+    curr_dir_name="$(basename "$PWD")"
+    docker pull abhishek1009/dev:latest
+    docker run -it --rm -v "$PWD":/workspace/"$curr_dir_name":rw -w /workspace/"$curr_dir_name" abhishek1009/dev zsh
+}
+
+# create a fuzzy finder with 3 options: sandbox, sandbox_curr, sandbox_dev
+sandbox_fuzzy_finder() {
+    declare -a options=("sandbox" "sandbox_curr" "sandbox_dev")
+    selected=$(printf "%s\n" "${options[@]}" | fzf --prompt="Select an option: ")
+    
+    case $selected in
+        "sandbox")
+            sandbox
+            ;;
+        "sandbox_curr")
+            sandbox_curr
+            ;;
+        "sandbox_dev")
+            sandbox_dev
+            ;;
+        *)
+            echo "Invalid selection"
+            ;;
+    esac
+}
+
+# Bind Ctrl+b to run sandbox_fuzzy_finder() in supported shells (bash/zsh)
 if [[ -n $ZSH_VERSION ]]; then
-  bindkey -s '^H' 'sandbox_curr\n'
+  bindkey -s '^B' 'sandbox_fuzzy_finder\n'
 elif [[ -n $BASH_VERSION ]]; then
-  bind -x '"\C-h":sandbox_curr'
+  bind -x '"\C-b":sandbox_fuzzy_finder'
 fi
