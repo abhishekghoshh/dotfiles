13eaad626be8a32aed243cd08e436604b2427e85
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2025-06-20 12:43:01 +0530

    [Abhishek] typo fixed

--- custom-shell-scripts/sandbox.sh
+++ custom-shell-scripts/sandbox.sh
@@ -36,7 +36,42 @@ sandbox() {
     "Dev Sandbox environment with new temporary directory"
     "Dev Sandbox environment in the current directory"
   )
-  selected=$(printf "%s\n" "${options[@]}" | fzf --prompt="Select an option: " --preview-window=right:60%:wrap,border-sharp --height=100% --layout=reverse --border=rounded --info=inline --style full)
+
+  preview_cmd='
+    case {} in
+      "Sandbox environment with new temporary directory")
+        rand_dir="sandbox-<random>"
+        echo "docker pull abhishek1009/sandbox:latest"
+        echo "mkdir \"\$PWD/\$rand_dir\""
+        echo "docker run -it --rm -v \"\$PWD/\$rand_dir\":/workspace/sandbox:rw -w /workspace/sandbox abhishek1009/sandbox zsh"
+        ;;
+      "Sandbox environment in the current directory")
+        curr_dir_name="$(basename \"$PWD\")"
+        echo "docker pull abhishek1009/sandbox:latest"
+        echo "docker run -it --rm -v \"\$PWD\":/workspace/\"\$curr_dir_name\":rw -w /workspace/\"\$curr_dir_name\" abhishek1009/sandbox zsh"
+        ;;
+      "Dev Sandbox environment with new temporary directory")
+        rand_dir="sandbox-<random>"
+        echo "docker pull abhishek1009/dev-sandbox:latest"
+        echo "mkdir \"\$PWD/\$rand_dir\""
+        echo "docker run -it --rm -v \"\$PWD/\$rand_dir\":/workspace/dev-sandbox:rw -w /workspace/dev-sandbox abhishek1009/dev-sandbox zsh"
+        ;;
+      "Dev Sandbox environment in the current directory")
+        curr_dir_name="$(basename \"$PWD\")"
+        echo "docker pull abhishek1009/dev-sandbox:latest"
+        echo "docker run -it --rm -v \"\$PWD\":/workspace/\"\$curr_dir_name\":rw -w /workspace/\"\$curr_dir_name\" abhishek1009/dev-sandbox zsh"
+        ;;
+      *)
+        echo "No command"
+        ;;
+    esac
+  '
+
+  selected=$(printf "%s\n" "${options[@]}" | \
+    fzf --prompt="Select an option: " \
+        --preview="$preview_cmd" \
+        --preview-window=right:60%:wrap,border-sharp \
+        --height=100% --layout=reverse --border=rounded --info=inline --style full)
 
   case $selected in
     "Sandbox environment with new temporary directory")
